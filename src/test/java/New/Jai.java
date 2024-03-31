8e93e43eb21e85fcbfbe3bcf66718e16e74ee5a4
Atul <atulgawande0403@gmail.com>
2024-03-31 09:24:45 +0530

    Ok

---
+++ src/test/java/New/Jai.java
@@ -0,0 +1,8 @@
+package New;
+
+public class Jai {
+	public static void main(String[] args) {
+		System.out.println("jai hind");
+	}
+
+}
---
+++ src/test/java/New/New_Git.java
@@ -0,0 +1,8 @@
+package New;
+
+public class New_Git
+{
+public static void main(String[] args) {
+	System.out.println("Ok");
+}
+}
